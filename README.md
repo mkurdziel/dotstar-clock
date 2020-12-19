7ac79dbfa987a4cc5f4ca04adf921f0bc870ce70
mkurdziel <michael.kurdziel@gmail.com>
2020-12-19 00:59:54 +0000

    First attempt at displaying image

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dotstar-clock
+
+`sudo apt-get install libopenjp2-7`
+`sudo apt install libtiff5`
--- clock.py
+++ clock.py
@@ -3,12 +3,16 @@ import math
 import random
 import board
 import adafruit_dotstar as dotstar
+from PIL import Image, ImageOps
 
 panelCount = 4
 panelWidth = 8
 panelHeight = 8
 panelTotal = panelWidth * panelHeight
 dotCount = panelTotal * panelCount
+asciiDir = './ascii'
+
+FILENAME = asciiDir + '/' + '48.png'
 
 dots = dotstar.DotStar(
         board.SCK, 
@@ -18,6 +22,28 @@ dots = dotstar.DotStar(
         brightness=0.1
     )
 
+IMG_RAW = Image.open(FILENAME).convert("RGB")
+IMG = ImageOps.invert(IMG_RAW)
+PIXELS = IMG.load()
+WIDTH = IMG.size[0]
+HEIGHT = IMG.size[1]
+print("%dx%d pixels" % IMG.size)
+
+
+print("Allocating...")
+COLUMN = [0 for x in range(WIDTH)]
+for x in range(WIDTH):
+    COLUMN[x] = [[0, 0, 0, 0] for _ in range(HEIGHT)]
+
+print("Converting...")
+for x in range(WIDTH):  # For each column of image
+    for y in range(HEIGHT):  # For each pixel in column
+        value = PIXELS[x, y]  # Read RGB pixel in image
+        COLUMN[x][y][0] = value[0]  # Gamma-corrected R
+        COLUMN[x][y][1] = value[1]  # Gamma-corrected G
+        COLUMN[x][y][2] = value[2]  # Gamma-corrected B
+        COLUMN[x][y][3] = 1.0  # Brightness
+        #print ('X ' + str(x) + ' Y ' + str(y) + ' value: ' + str(value[0]))
 
 # HELPERS
 # a random color 0 -> 192
@@ -32,20 +58,33 @@ def random_color():
     #dots.show()
     #time.sleep(0.0)
 
-def getPixel(col, row):
+#
+# Draw a given image matrix at a given offset
+#
+def showImage(img, x, y):
+    for x in range(len(img)):  # For each column of image
+        for y in range(len(img[0])):  # For each pixel in column
+            value = img[x][y]  # Read RGB pixel in image
+            pixel = getPixel(x,y)
+            dots[pixel] = [value[0], value[1], value[2]]
+    dots.show()
+
+def getPixel1(col, row):
     pX = col % panelWidth
-    print('pX: ' + str(pX))
     pN = math.floor((col + 1) / panelWidth)
-    print('pN: ' + str(pN))
     pixel = (panelTotal * pN) + (panelWidth * row + pX)
     return pixel
 
+def getPixel(col, row):
+    pixel = (panelHeight * col) + row
+    return pixel
+
+
+print("Clearing screen")
 for x in range(0, dotCount):
     dots[x] = (0, 0, 0)
-
 dots.show()
-#dots[0] = (255, 255, 0)
+
+showImage(COLUMN, 0, 0)
 
 
-print("2, 4, " + str(getPixel(2, 4)))
-print("11, 2, " + str(getPixel(11, 2)))
